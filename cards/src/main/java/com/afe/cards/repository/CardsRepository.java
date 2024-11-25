2012341a088358d25f055dd646d75631a5d6fd76
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2024-11-25 08:25:59 +0300

    Cards repository added

---
+++ cards/src/main/java/com/afe/cards/repository/CardsRepository.java
@@ -0,0 +1,13 @@
+package com.afe.cards.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.afe.cards.entity.Cards;
+
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+}
