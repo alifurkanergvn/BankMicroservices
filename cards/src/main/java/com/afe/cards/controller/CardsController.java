8c68ac019d28abf1531ca9598df68f5d7b8f9b93
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2024-11-27 22:33:38 +0300

    Cards added new version swagger and renamed openapi documantation

--- cards/src/main/java/com/afe/cards/controller/CardsController.java
+++ cards/src/main/java/com/afe/cards/controller/CardsController.java
@@ -30,8 +30,8 @@ import com.afe.cards.dto.ResponseDto;
 import com.afe.cards.service.ICardsService;
 
 @Tag(
-        name = "CRUD REST APIs for Cards in EazyBank",
-        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details"
+        name = "CRUD REST APIs for Cards in MyBank",
+        description = "CRUD REST APIs in MyBank to CREATE, UPDATE, FETCH AND DELETE card details"
 )
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -43,7 +43,7 @@ public class CardsController {
 
     @Operation(
             summary = "Create Card REST API",
-            description = "REST API to create new Card inside EazyBank"
+            description = "REST API to create new Card inside MyBank"
     )
     @ApiResponses({
             @ApiResponse(
