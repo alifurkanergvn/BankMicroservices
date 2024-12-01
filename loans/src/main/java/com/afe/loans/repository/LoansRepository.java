bbe81a4322a60679be690b332631a984983f2516
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2024-12-01 21:27:56 +0300

    Loans repository class added

---
+++ loans/src/main/java/com/afe/loans/repository/LoansRepository.java
@@ -0,0 +1,13 @@
+package com.afe.loans.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.afe.loans.dto.LoansDto;
+import com.afe.loans.entity.Loans;
+
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+    Optional<Loans> findByLoanNumber(String loanNumber);
+}
