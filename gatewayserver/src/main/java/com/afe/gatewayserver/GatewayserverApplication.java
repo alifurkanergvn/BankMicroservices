beee02303234ef6896cced3004cf57033afc6d62
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2025-09-30 00:37:34 +0300

    Implementing Custom Routing using Spring Cloud Gateway

--- gatewayserver/src/main/java/com/afe/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/afe/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,31 @@ package com.afe.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	// http://localhost:8072/actuator/gateway/routes ->
+	@Bean
+	public RouteLocator myBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p.path("/mybank/accounts/**")
+						.filters(f -> f.rewritePath("/mybank/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p.path("/mybank/loans/**")
+						.filters(f -> f.rewritePath("/mybank/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p -> p.path("/mybank/cards/**")
+						.filters(f -> f.rewritePath("/mybank/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.build();
 	}
 
 }
