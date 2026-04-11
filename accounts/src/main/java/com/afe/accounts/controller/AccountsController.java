86bda951bab8a98b4c78c900b33844485009e6f2
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2026-04-11 19:09:54 +0300

    Implementing RateLimiter pattern in accounts

--- accounts/src/main/java/com/afe/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/afe/accounts/controller/AccountsController.java
@@ -1,5 +1,6 @@
 package com.afe.accounts.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -241,11 +242,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
